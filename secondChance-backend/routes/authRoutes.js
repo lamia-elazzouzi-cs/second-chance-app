76dbefb3a5cc49cfb5044487c5738dc4d861f041
lamia.elazzouzi.cs <lamia.elazzouzi.cs@gmail.com>
2024-08-19 07:41:58 -0400

    Create /update endpoint

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -10,6 +10,8 @@ const JWT_SECRET = `${process.env.JWT_SECRET}` || "thisisasecretkey";
 const pino = require('pino');  // Import Pino logger
 const logger = pino();  // Create a Pino logger instance
 
+const { body, validationResult } = require('express-validator'); // For user input validation
+
 router.post('/register', async (req, res) => {
     try {
         // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
@@ -58,7 +60,7 @@ router.post('/login', async (req, res) => {
         // Task 3: Check for user credentials in database
         const { email, password } = req.body;
         const appUser = await collection.findOne({ email: email });
-        
+
         // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
         if (appUser) {
             let result = await bcrypt.compare(password, appUser.password);
@@ -94,23 +96,56 @@ router.post('/login', async (req, res) => {
 });
 
 // {Insert it along with other imports} Task 1: Use the `body`,`validationResult` from `express-validator` for input validation
-
 router.put('/update', async (req, res) => {
     // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
-try {
-    // Task 3: Check if `email` is present in the header and throw an appropriate error message if it is not present
-    // Task 4: Connect to MongoDB
-    // Task 5: Find the user credentials in database
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() })
+    }
 
-    existingUser.updatedAt = new Date();
+    try {
+        // Task 3: Check if `email` is present in the header and throw an appropriate error message if it is not present
+        const email = req.headers.email;
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
 
-    // Task 6: Update the user credentials in the database
-    // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
-    res.json({authtoken});
-} catch (e) {
-     return res.status(500).send('Internal server error');
+        // Task 4: Connect to MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        
+        // Task 5: Find the user credentials in database
+        const existingUser = await collection.findOne({ email });
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: "/update error - User not found" });
+        }
 
-}
+        existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
+
+        // Task 6: Update the user credentials in the database
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+       
+        // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
+        const payload = {
+            user: { id: updatedUser._id.toString(), }
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User updated successfully');
+        res.json({ authtoken });
+
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+
+    }
 });
 
 module.exports = router;
